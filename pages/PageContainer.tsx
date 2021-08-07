261dc7c5e4daee828dfbd429777e0256b605f2ed
Muhab Alwan <alwanmuhab@gmail.com>
2021-08-07 13:20:09 +0200

    Simplify code, fix navigation, set up pages rendering using slugs

--- pages/PageContainer.tsx
+++ pages/PageContainer.tsx
@@ -1,44 +1,46 @@
-import { useState } from "react";
-import Layout from "../components/Layout";
-import { PostsOrPages } from '@tryghost/content-api';
-import { INavList } from "./_app";
-
+import { useEffect, useState } from "react";
+import { PostsOrPages, PostOrPage } from '@tryghost/content-api';
 
 interface IPageContainerProps {
   pageSlug: string,
   pages: PostsOrPages,
-  navList: INavList
 }
 
 
 export interface ISelectedLink {
-  id: string,
-  title?: string,
-  slug: string,
+  label: string,
+  url: string,
 }
 
 const Page = (props: IPageContainerProps) => {
-    const { pageSlug, pages, navList } = props;
-    const [selectedPageId, setSelectedPageId] = useState("60827a29647814089c944f5b"); // TODO: fix this: this is the Home page ID!
+  const { pageSlug } = props;
+  const [selectedPage, setSelecetedPage] = useState<PostOrPage>();
 
-    const page = props?.pages?.find(page => {
-        return page.slug === props.pageSlug
-      });
 
-    const getSelectedLink = (selectedLink: ISelectedLink) => {
-      selectedLink.id && setSelectedPageId(selectedLink.id);
+  useEffect(() => {
+    const pageFromSlug = getPageFromSlug(pageSlug);
+    setSelecetedPage(pageFromSlug);
+  });
+
+  const getPageFromSlug = (pageSlug: string) => {
+    const selectedPage = props.pages.find(page => {
+      if (page?.slug?.toLowerCase() === pageSlug.toLowerCase()) {
+        return page;
       }
+    });
+    return selectedPage;
+  }
 
-    const createMarkup = (html: string) => {
-        return {__html: html};
-    }
+  const createMarkup = (html: string) => {
+    return { __html: html };
+  }
 
-    return (
-    <Layout pages={props.pages} getSelectedLink={getSelectedLink} navList={navList}>
+  return (
+    <>
       {
-        page?.html ? <div dangerouslySetInnerHTML={createMarkup(page?.html)} /> : false
+        selectedPage?.html ? <div dangerouslySetInnerHTML={createMarkup(selectedPage?.html)} /> : false
       }
-    </Layout>)
+    </>)
 }
 
 
